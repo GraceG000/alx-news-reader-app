e3c3ed322fb3b87df74240f7a4cc64bebc12700a
Grace Ghansah <graceghansah2000@gmail.com>
2026-02-15 21:57:59 +0000

    feat: modified the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,13 @@ function App() {
   return (
     <>
   
-      <h1 className="text-5xl text-blue-500 font-bold ">
-    Hello world!
+  <h1 className="text-5xl text-blue-500 font-bold ">
+    The Daily Tea News Site!!!
   </h1>
+  <div className='mt-2'>
+      <p>All the latest news and updates, all in one place</p>
+  </div>
+  
     </>
   )
 }
