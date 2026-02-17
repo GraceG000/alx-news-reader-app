b2159638faa9c3d00ac1e1b74a34763cfca0ec63
Grace Ghansah <graceghansah2000@gmail.com>
2026-02-17 11:13:22 +0000

    update: added a fall back image incase articles do not have readily available images

--- src/components/NewsArticleCard.jsx
+++ src/components/NewsArticleCard.jsx
@@ -11,7 +11,11 @@ const NewsArticleCardComponent = ({
         <>
          <div className="bg-slate-100 rounded-lg shadow-lg p-4">
             <div className="font-bold">{title}</div>
-            <div>{image}</div>
+            <div> <img 
+                src={image} 
+                alt={title || "News image"} 
+                className="w-full h-48 object-cover mb-2 rounded" 
+            /></div>
             <div>{description}</div>
             <div className="text-sm text-gray-500">{published}</div>
             <div className="text-sm text-gray-500">{author}</div>
